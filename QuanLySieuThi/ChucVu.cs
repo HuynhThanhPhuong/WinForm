584ea55b6aef411b050ec7096f9071e2a69d4cd4
Loi-Tran <loi236711@gmail.com>
2022-03-03 16:35:13 +0700

    Phương

--- QuanLySieuThi/ChucVu.cs
+++ QuanLySieuThi/ChucVu.cs
@@ -8,7 +8,7 @@ namespace QuanLySieuThi
 {
     class ChucVu
     {
-        public int MaCV { get; set; }
+        public int MaCONGCONG { get; set; }
         public string TenCV { get; set; }
         public ChucVu() { }
         public ChucVu(int macv,string tencv)
